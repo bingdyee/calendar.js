51cc66b0ea78cf6fc4f1719c19236d532f516922
Noa Swartz <fetaxyu@gmail.com>
2019-04-11 21:25:12 +0800

    first commit

---
+++ codespot-bc/codespot/main.cc
@@ -0,0 +1,20 @@
+﻿/* Copyleft 2019 The CodeSpot-BC Authors. Some Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+
+Eigen/grpc/Crypto++
+
+@author: Noa Swartz
+@email: fetaxyu@gmail.com
+@date: 2019-04-11
+==============================================================================*/
+
+#include <iostream>
+
+#include "codespot/utils/cryptography.h"
+
+int main(int args, char** argv) 
+{
+
+
+	return 0;
+}
---
+++ codespot-bc/codespot/utils/cryptography.h
@@ -0,0 +1,19 @@
+﻿/* Copyleft 2019 The CodeSpot-BC Authors. Some Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+
+@author: Noa Swartz
+@email: fetaxyu@gmail.com
+@date: 2019-04-11
+==============================================================================*/
+
+#ifndef CODESPOT_UTILS_CRYPTOGRAPHY_H_
+#define CODESPOT_UTILS_CRYPTOGRAPHY_H_
+
+namespace codespot {
+
+
+
+} // end namespace codespot
+
+#endif // !CODESPOT_UTILS_CRYPTOGRAPHY_H_
+
